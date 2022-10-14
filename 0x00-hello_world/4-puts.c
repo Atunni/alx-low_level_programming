b79f8c7bd8d564c035a2b682426ed5a802cb4d68
Mariam Azeez <azeezmariam002@gmail.com>
2022-10-13 18:45:32 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: This returns 0
+*/
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzl / n");
+	return (0);
+}
+
