62aa3f8edc75697abf1120b8348ecc9586fe27b6
Mariam Azeez <azeezmariam002@gmail.com>
2022-10-15 08:00:12 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit number
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		putchar(y);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
