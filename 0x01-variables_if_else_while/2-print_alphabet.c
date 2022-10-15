8aeffc40642eb2c26c4d65a129be38b9cf411f3b
Mariam Azeez <azeezmariam002@gmail.com>
2022-10-15 00:14:36 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - lower case alphabet
+ *Return: Returns 0
+ */
+
+int main(void)
+{
+	char LC
+
+	for (char LC = 'a'; LC <= 'z'; LC++)
+	{
+		putchar("lc)
+	}
+	putchar('/n');
+	return (0);
+
+}
