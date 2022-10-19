07bc44a348a50e3d4a88d8352e34a60d9d1288ac
Mariam Azeez <azeezmariam002@gmail.com>
2022-10-19 04:47:45 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase 10 times
+ *
+ *Return: returns 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 0;
+	char y;
+
+	while (x++ <= 9)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+			_putchar(y);
+	}
+
+	_putchar('\n');
+}
