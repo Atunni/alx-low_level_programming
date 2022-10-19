aab83c9ac25a8dfbf2714aeb69701feb1a54e47d
Mariam Azeez <azeezmariam002@gmail.com>
2022-10-18 20:47:14 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lower case
+ *Return: Returns 0
+ */
+
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+
+	{
+		_putchar('a');
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
