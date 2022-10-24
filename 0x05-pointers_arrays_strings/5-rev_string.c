ccd158f10404507d7dedaa72c9687210e95e488f
Mariam Azeez <azeezmariam002@gmail.com>
2022-10-24 15:16:42 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,30 @@
 #include "main.h"
 
 /**
- * rev_string - prints functions that reverses a spring
- * @s: input string
- * Return: no return
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
  */
-
 void rev_string(char *s)
 {
-	int count = 0;
+	int count = 0, i, j;
+	char *str, temp;
 
 	while (count >= 0)
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
+		if (s[count] == '\0')
 			break;
-		}
-		if (count % 2 == 0)
-			_putchar(str[count]);
 		count++;
+	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
 	}
 }
