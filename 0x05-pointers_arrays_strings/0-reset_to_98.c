a6a978471f9dd7e8c9afbce00059493da0666c07
Mariam Azeez <azeezmariam002@gmail.com>
2022-10-24 13:37:40 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer toba parameter and update its value
+ * @n: input integer
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
